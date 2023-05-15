ae9d7711055b038a39e225a7fe533c2ca9583f0c
Vojtěch Petrák <132489256+vojtech-petrak@users.noreply.github.com>
2023-05-15 22:10:28 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,18 @@
+use std::io::stdout;
+use crossterm::{
+    ExecutableCommand,
+    terminal,
+};
+use draw::{
+    utils::SCREEN_SIZE,
+    event::event_capture,
+};
 
+fn main() {
+    terminal::enable_raw_mode().unwrap();
+    stdout().execute(terminal::EnterAlternateScreen).unwrap();
+
+    let (x, y): (u16, u16) = terminal::size().unwrap();
+    unsafe { SCREEN_SIZE = [x as usize >> 1, y as usize - 3] }
+    event_capture(None);
+}
